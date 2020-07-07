6963257099eb5b8667c62f08811961d2a8c3f86a
Fergus McClean <fergus.mcclean@ncl.ac.uk>
2020-07-07 13:48:03 +0100

    added return periods and legend

--- src/map.js
+++ src/map.js
@@ -1,7 +1,7 @@
 
 angular.module('app', [])
     .service('MapService', function () {
-        const map = L.map('mapid').setView([2.631, 112.975], 10);
+        const map = L.map('mapid').setView([3.0792, 113.3956], 10);
 
         L.tileLayer('https://c.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 18,
@@ -9,18 +9,44 @@ angular.module('app', [])
         }).addTo(map);
 
         let geotiffLayer;
+        const displayMin = 1
+        const displayMax = 5
+        const normalize = function(val) { return (val - displayMin) / (displayMax - displayMin); }
+
+        const colours = ["#2b83ba", "#2b4Dba", "#0000ff", "#000072", "#000000"]
+        const labels = [10, 100, 200, 1000, 10000]
+        plotty.addColorScale("return-periods", colours,
+            [1, 2, 3, 4, 5].map(normalize));
+
+        const legend = L.control({position: 'topleft'});
+
+        legend.onAdd = function (map) {
+
+            let div = L.DomUtil.create('div', 'info legend')
+            div.innerHTML += '<h5>Return Period (yrs)</h5>'
+
+            // loop through our density intervals and generate a label with a colored square for each interval
+            for (let i = 0; i < labels.length; i++) {
+                div.innerHTML += '<i style="background-color:' + colours[i] + '">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</i> ' + labels[i] + '<br>';
+            }
+
+            return div;
+        };
+
+        legend.addTo(map);
+
 
         return {
             getMap: function () {return map},
             updateOpacity: function (opacity) {geotiffLayer.setOpacity(opacity)},
             updateGeotiffLayer: function (run) {
                 if (geotiffLayer) {geotiffLayer.remove()}
-                geotiffLayer = L.leafletGeotiff(`data/run_${run.id}_maxdepth.tiff`, {
+                geotiffLayer = L.leafletGeotiff(`data/return_periods.tif`, {
                     renderer: L.LeafletGeotiff.plotty({
                         clampLow: false,
-                        displayMin: 10,
-                        displayMax: 1000,
-                        colorScale: 'viridis'
+                        displayMin: displayMin,
+                        displayMax: displayMax,
+                        colorScale: 'return-periods'
                     })
                 }).addTo(map);
                 geotiffLayer.setOpacity(0.5)
